4b94035237476c948855fb12e1adcf6b997f3e88
ChristyanPB <hectorbravosilva@gmail.com>
2024-10-06 01:22:29 -0500

    Job and Resume Files added. Program modified

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Coopac Cajasol";
+        job1._jobTitle = "Credit Analist";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Grupo Financiero JMG";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Christyan Bravo";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
